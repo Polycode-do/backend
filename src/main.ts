6d605b756956e8ec468f3da1411a75f3aeef1c68
Alexandre Gomez <gomez.a.corneille@gmail.com>
2022-05-02 17:54:28 +0200

    docs: Swagger base configuration
    Added a base configuration for a Swagger documentation.
    
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,19 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
+
+  //Swagger Configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Polycode API')
+    .setDescription('API description for Polycode main API')
+    .setVersion(process.env.APP_VERSION || '0.1.0')
+    .addTag('user', 'User related operations')
+    .addTag('challenge', 'Challenge related operations')
+    .addTag('exercise', 'Exercise related operations')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
