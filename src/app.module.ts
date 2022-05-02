54981332df1486c67a9737e534ff2f614360686f
Alexandre Gomez <gomez.a.corneille@gmail.com>
2022-05-02 09:42:43 +0200

    feat: Create base http server architecture
    Created a base architecture of a http server to build features on top of
    
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ChallengeModule } from './challenge/challenge.module';
+import { UserModule } from './user/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ExerciseModule } from './exercise/exercise.module';
+
+@Module({
+  imports: [
+    ChallengeModule,
+    UserModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'postgres',
+      autoLoadModels: true,
+    }),
+    ExerciseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
